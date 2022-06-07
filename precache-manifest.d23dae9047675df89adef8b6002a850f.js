b1e7a46daa987026ed76b56d0b5d0a240dc5614e
Keane <keanetay@gmail.com>
2022-06-07 14:12:40 +0800

    Updates

---
+++ precache-manifest.d23dae9047675df89adef8b6002a850f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c6038604bee0d095a1",
+    "url": "/todo-app-v2/static/css/main.51bf200e.chunk.css"
+  },
+  {
+    "revision": "a0c6038604bee0d095a1",
+    "url": "/todo-app-v2/static/js/main.a0c60386.chunk.js"
+  },
+  {
+    "revision": "2fdf0ae7038f18776d35",
+    "url": "/todo-app-v2/static/css/1.2b6a56e6.chunk.css"
+  },
+  {
+    "revision": "2fdf0ae7038f18776d35",
+    "url": "/todo-app-v2/static/js/1.2fdf0ae7.chunk.js"
+  },
+  {
+    "revision": "f20fa18fb2506d9ea8e4",
+    "url": "/todo-app-v2/static/js/runtime~main.f20fa18f.js"
+  },
+  {
+    "revision": "0704733dbd791747c2620f009d55fac5",
+    "url": "/todo-app-v2/index.html"
+  }
+];
